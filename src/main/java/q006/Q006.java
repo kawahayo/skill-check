ed938731426e6d974c58b5cf5b60ae843c7a40fa
kawahayo <firstbrillouinzone@gmail.com>
2020-05-08 18:38:58 +0900

    q006

--- src/main/java/q006/Q006.java
+++ src/main/java/q006/Q006.java
@@ -3,9 +3,14 @@ package q006;
 import q006.value.DecimalValue;
 import q006.value.IValue;
 import q006.value.PlusValue;
-
+import q006.value.MinusValue;
+import q006.value.MultipleValue;
+import q006.value.DivideValue;
+import java.math.BigDecimal;
+import java.util.Stack;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * Q006 空気を読んで改修
@@ -29,6 +34,31 @@ import java.util.List;
  * （または -1.00 など、小数点に0がついてもよい）
  */
 public class Q006 {
+    public static void main(String[] args) {
+        // TODO:計算式入力 (Stringで式を指定すると問題なく結果が出力されることを確認）
+        /* 
+        System.out.println("入力)　");
+        Scanner scan = new Scanner(System.in);
+        String formulaStr = scan.nextLine();
+         */
+
+        // 暫定で計算式はここで定義
+        String formulaStr = "3 1.1 0.9 + 2.0 * -";
+
+        // 入力された計算式テキストを分解
+        List<IValue> formulaStrList = parseLine(formulaStr);
+
+        // 計算実行
+        Stack<BigDecimal> calcRet = new Stack<BigDecimal>();
+        for(int i=0; i < formulaStrList.size(); i++) {
+            // DecimalValue、XXXXValueでそれぞれexecuteを実装しているので、ループ回すだけ
+            formulaStrList.get(i).execute(calcRet);
+        }
+
+        // 計算結果出力
+        System.out.println("出力)　" + calcRet.pop());
+    }
+        
     /**
      * 逆ポーランドで記載された1行のテキストを分解する
      * @param lineText 1行テキスト
@@ -38,10 +68,18 @@ public class Q006 {
         List<IValue> resultList = new ArrayList<>();
         // 空白文字で区切ってループする
         for (String text: lineText.split("[\\s]+")) {
-            // TODO 一部処理だけ実装
             switch (text) {
                 case "+":   // 足し算
                     resultList.add(new PlusValue());
+                    break;
+                case "-":   // 引き算
+                    resultList.add(new MinusValue());
+                    break;
+                case "*":   // 掛け算
+                    resultList.add(new MultipleValue());
+                    break;
+                case "/":   // 割り算
+                    resultList.add(new DivideValue());
                     break;
                 default:    // その他は数値として扱う
                     resultList.add(new DecimalValue(text));
